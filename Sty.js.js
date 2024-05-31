3c9a15110258e73bba7f5941b1555c2128d18b33
Dela Cruz, Luis Adam D <luisadamdelacruz8@gmail.com>
2024-05-31 12:05:28 +0800

    Add files via upload

---
+++ Sty.js.js
@@ -0,0 +1,51 @@
+function input1() {
+    document.getElementById("label1").style.color="Red";
+    document.getElementById("label2").style.color="Red";
+    document.getElementById("label3").style.color="Green";
+    
+  }
+  function input2() {
+    document.getElementById("label4").style.color="Green";
+    document.getElementById("label5").style.color="Red";
+    document.getElementById("label6").style.color="Red";
+  }
+  function input3() {
+    document.getElementById("label7").style.color="Green";
+    document.getElementById("label8").style.color="Red";
+    document.getElementById("label9").style.color="Red";
+  }
+  function input4() {
+    document.getElementById("label10").style.color="Red";
+    document.getElementById("label11").style.color="Red";
+    document.getElementById("label12").style.color="Green";
+  }
+  function input5() {
+    document.getElementById("label13").style.color="Red";
+    document.getElementById("label14").style.color="Red";
+    document.getElementById("label15").style.color="Green";
+  }
+  function input6() {
+    document.getElementById("label16").style.color="Red";
+    document.getElementById("label17").style.color="Red";
+    document.getElementById("label18").style.color="Green";
+  }
+  function input7() {
+    document.getElementById("label19").style.color="Green";
+    document.getElementById("label20").style.color="Red";
+    document.getElementById("label21").style.color="Red";
+  }
+  function input8() {
+    document.getElementById("label22").style.color="Red";
+    document.getElementById("label23").style.color="Red";
+    document.getElementById("label24").style.color="Green";
+  }
+  function input9() {
+    document.getElementById("label25").style.color="Red";
+    document.getElementById("label26").style.color="Red";
+    document.getElementById("label27").style.color="Green";
+  }
+  function input10() {
+    document.getElementById("label28").style.color="Red";
+    document.getElementById("label29").style.color="Red";
+    document.getElementById("label30").style.color="Green";
+  }
